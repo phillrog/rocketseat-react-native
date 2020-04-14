c7a21362df101493909782cf761835af2b10441a
fh.proger <phillipe.souza@rte.com.br>
2020-04-13 22:03:57 -0300

    Estilizando Header e StatusBar

---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,4 @@
+import { StatusBar} from 'react-native';
+
+StatusBar.setBackgroundColor('#DD552F');
+StatusBar.setBarStyle('light-content');
--- src/routes.js
+++ src/routes.js
@@ -2,23 +2,59 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
+import { StyleSheet, View, Text } from 'react-native';
+
 import Product from './pages/main';
 import Detail from './pages/detail';
+import Icon from '@material-ui/core/Icon';
 
 const Stack = createStackNavigator()
 
 function Routes() {
   return (
-    <NavigationContainer>
+    <NavigationContainer options={{backgroundColor: "green"}}>
       <Stack.Navigator initialRouteName='Product'
         screenOptions={{
-          gestureEnabled: true
+          gestureEnabled: true,
+          title: "JSHunt",
+          headerTitleAlign: "center",          
         }}>
-        <Stack.Screen name='Product' component={Product} options={{ title: 'Produto' }} />
+        <Stack.Screen 
+          name='Product' 
+          component={Product} 
+          options={{
+            headerStyle: ({ backgroundColor: "#DA552F"}),
+            headerTintColor: "#FFF",
+            headerTitle: () => (
+              <View style={styles.container}>
+                <View>
+                  <Text style={styles.headerTitle}>JSHunt</Text>
+                </View>
+              </View>
+            )            
+          }}
+        />
         <Stack.Screen name='Detail' component={Detail} options={{ title: 'Detalhes' }} />
       </Stack.Navigator>
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  headerTitle: {
+    fontSize: 24,
+    color: "#FFF",
+    letterSpacing: 1,
+  },
+  icon: {
+    marginLeft: 10
+  }
+});
 
 export default Routes;
